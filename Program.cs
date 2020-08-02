a14533754a75e70b648efad9a407f248b9abb9d3
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-08-02 07:36:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Code_Challenge_Day_16_Exceptions__String_to_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            try
+            {
+                Console.WriteLine(int.Parse(s));
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
